36c4b5d805dfd74090986563e626e4b62cd601a5
thomaskeady <thomas.keady@jhu.edu>
2019-04-21 15:17:06 -0400

    Adding custom branching rule to test_short

--- tests/my_test_short.py
+++ tests/my_test_short.py
@@ -22,6 +22,46 @@ solufilepath = 'check/testset/one.solu'
 print("******** After path declarations ********")
 
 
+class MyBranching(Branchrule):
+
+    def __init__(self, model, cont):
+        self.model = model
+        self.cont = cont
+        self.count = 0
+        self.was_called_val = False
+        self.was_called_int = False
+
+    def branchexeclp(self, allowaddcons):
+        self.model.getLPBranchCands()
+        self.count += 1
+        if self.count >= 2:
+            return {"result": SCIP_RESULT.DIDNOTRUN}
+        assert allowaddcons
+
+        assert not self.model.inRepropagation()
+        assert not self.model.inProbing()
+        self.model.startProbing()
+        assert not self.model.isObjChangedProbing()
+        self.model.fixVarProbing(self.cont, 2.0)
+        self.model.constructLP()
+        self.model.solveProbingLP()
+        self.model.getLPObjVal()
+        self.model.endProbing()
+
+        self.integral = self.model.getLPBranchCands()[0][0]
+
+        if self.count == 1:
+            down, eq, up = self.model.branchVarVal(self.cont, 1.3)
+            self.model.chgVarLbNode(down, self.cont, -1.5)
+            self.model.chgVarUbNode(up, self.cont, 3.0)
+            self.was_called_val = True
+            down2, eq2, up2 = self.model.branchVar(self.integral)
+            self.was_called_int = True
+            self.model.createChild(6, 7)
+            return {"result": SCIP_RESULT.BRANCHED}
+
+
+
 if not all(os.path.isfile(fn) for fn in [testsetpath, solufilepath]):
     if pytest.__version__ < "3.0.0":
         pytest.skip("Files for testset `short` not found (symlink missing?)")
@@ -84,6 +124,13 @@ def test_instance(instance):
     s = Model()
     s.hideOutput()
     s.readProblem(instance)
+
+    # Copy pasta from test_branch_probin_lp
+    my_branchrule = MyBranching(s)
+    m.includeBranchrule(my_branchrule, "test branch", "test branching and probing and lp functions",
+                    priority=10000000, maxdepth=3, maxbounddist=1)
+
+
     s.optimize()
 
     print("******** Done optimizing ********")
